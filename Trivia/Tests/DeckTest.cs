9b83b3824f905078a953fcf42be1409cd3519c16
Rémy Robert <45172609+RemyRobert@users.noreply.github.com>
2022-07-26 14:39:04 +0200

    implementation du test de creation d'un deck pop

---
+++ Trivia/Tests/DeckTest.cs
@@ -0,0 +1,21 @@
+﻿using Trivia;
+using Xunit;
+
+namespace Tests
+{
+    public class DeckTest
+    {
+        [Fact]
+        public void Create_a_pop_deck()
+        {
+            var deck = new Deck(Question.Categories.Pop, 40);
+
+            var question = deck.Draw();
+
+            Assert.Contains("Pop", question.ToString());
+        }
+
+        //Draw une carte donne bien la première, qui n'y est plus après
+        
+    }
+}
